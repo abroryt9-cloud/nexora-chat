68d67688e3bd6bb5bfb8238cbe048ab410a465ab
abroryt9-cloud <abroryt9@gmail.com>
2026-03-22 15:08:17 +0300

    Create gifs.ts

---
+++ shared/constants/gifs.ts
@@ -0,0 +1,4 @@
+// Это просто список категорий для Tenor API
+export const gifCategories = [
+  'trending', 'happy', 'funny', 'love', 'cool', 'animals', 'reaction', 'sad', 'angry', 'celebration'
+];
