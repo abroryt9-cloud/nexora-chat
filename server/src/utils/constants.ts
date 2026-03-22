d02eb74f7b1be07f59e251178f7300db42657243
abroryt9-cloud <abroryt9@gmail.com>
2026-03-22 16:38:13 +0300

    Create constants.ts

---
+++ server/src/utils/constants.ts
@@ -0,0 +1,4 @@
+export const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5MB
+export const SUPPORTED_IMAGE_TYPES = ['image/jpeg', 'image/png', 'image/gif', 'image/webp'];
+export const DEFAULT_AVATAR = 'https://ui-avatars.com/api/?background=random';
+export const NXR_DECIMALS = 18;
