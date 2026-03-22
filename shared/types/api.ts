00216e40f54224e9ecdcc7309b888ac69cbeaf34
abroryt9-cloud <abroryt9@gmail.com>
2026-03-22 15:04:52 +0300

    Create api.ts

---
+++ shared/types/api.ts
@@ -0,0 +1,13 @@
+export interface ApiResponse<T = any> {
+  success: boolean;
+  data?: T;
+  message?: string;
+  errors?: any;
+}
+
+export interface PaginatedResponse<T> {
+  data: T[];
+  total: number;
+  page: number;
+  limit: number;
+}
